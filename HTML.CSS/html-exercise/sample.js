22455e43ca02606123ad41ea288a3ad288bea5e8
srod-85 <94401097+srod-85@users.noreply.github.com>
2022-01-12 15:23:25 -0800

    web page exercise

---
+++ HTML.CSS/html-exercise/sample.js
@@ -0,0 +1,27 @@
+
+let admin = 'Samantha';
+let name = 'Rikki';
+admin = name;
+name = admin;
+// alert(admin);
+// alert(name) // returns 'Rikki'
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'John';
+
+let sentence = `My name is Samantha, but I prefer to be called ${name}.`
+// alert(sentence);
+// alert(null)
+
+/*
+alert(typeof ('3'+5)); //returns string
+alert(typeof "3"+5); //returns string5
+alert(typeof null); //returns object
+alert(typeof undefined); //returns undefined
+alert((1 == 1)); //returns true
+alert(false); //returns false
+alert(1 == 1); //returns true
+alert(typeof Math); //returns object
+alert(typeof 3n); //returns bigint
+alert(typeof alert); //returns function
+*/
